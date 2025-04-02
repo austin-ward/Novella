05df93c3720d3094197da33fc73acb2f3ebafc6e
Austin Ward <wardaustin2001@gmail.com>
2025-04-02 13:21:57 -0400

    Update db.php
    Changing db.php for DB connection file to the books table.

--- php/db.php
+++ php/db.php
@@ -1,12 +1,13 @@
 <?php
-$host = 'localhost';
-$dbname = 'novella';
-$username = 'root';
-$password = ''; // or your password
+$servername = "localhost";
+$username = "your_db_username";
+$password = "your_db_password";
+$dbname = "your_db_name";
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
